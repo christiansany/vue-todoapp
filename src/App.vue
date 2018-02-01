93a94715d61f505cfd72b9abe2edd8968ca47dfe
christiansany <christian.sany@bluewin.ch>
2018-02-01 15:42:12 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+
+    <form v-on:submit.prevent="submitHandler">
+      <input v-model="newTodoValue" type="text" required>
+      <button type="submit">Add Todo</button>
+    </form>
+
+    <h2>Todos ({{ todosAmount }})</h2>
+
+    <TodoList v-bind:todos="todos" v-on:updateTodo="updateTodo"/>
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList'
+
+let i = 1;
+
 export default {
   name: 'app',
+  components: {
+    TodoList,
+  },
   data() {
     return {
-      msg: 'Hello World',
+      msg: 'Hello World!!!!',
+      newTodoValue: '',
+      todos: [
+        {
+          id: i++,
+          title: 'My first todo',
+          done: false,
+        },
+        {
+          id: i++,
+          title: 'My second todo',
+          done: false,
+        }
+      ]
     };
+  },
+  computed: {
+    todosAmount() {
+      return this.todos.length;
+    }
+  },
+  methods: {
+    submitHandler() {
+      this.todos.push({
+        id: i++,
+        title: this.newTodoValue,
+        done: false,
+      });
+      this.newTodoValue = '';
+    },
+    doneHandler(todo) {
+      // console.log(todo); // eslint-disable-line
+      todo.done = true;
+    },
+    updateTodo() {
+      
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 30px;
+
+  h1 {
+    font-size: 40px;
+  }
+}
+
+.is-done {
+  color: green;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li
+      v-for="todo in todos"
+      v-bind:key="todo.id"
+      v-bind:class="{ 'is-done': todo.done }"  
+    >
+      {{ todo.title }}
+      <template v-if="!todo.done">
+        <button v-on:click="doneHandler(todo)">DONE!</button>
+      </template>
+      <span v-else>YAY</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: [
+    'todos'
+  ],
+  methods: {
+    doneHandler(todo) {
+      this.$emit('updateTodo', {
+        action: 'done',
+        todo,
+      })
+    }
+  }
+}
+</script>
